388fc619afc785d16f20a84a90f8275fdd9ac52e
RohitLbd <rrlambade_m23@ee.vjti.ac.in>
2024-10-20 09:39:53 +0530

    register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'register', component: RegisterComponent,
+    },
+    // { 
+    //     path: 'about', component: AboutComponent,
+    //     children: [
+    //         {path: 'rating', outlet:'rate', component:RatingComponent},
+    //         {path: 'feedback', outlet:'feed', component:FeedbackComponent}
+    //     ]
+    // }
+];
