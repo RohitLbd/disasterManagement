24b38d9596cbedd50e30f5ddd90ebe7919019eda
RohitLbd <rrlambade_m23@ee.vjti.ac.in>
2024-10-20 13:16:25 +0530

    333

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,5 +12,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router:Router){}
 
+  goToPage2(){
+    this.router.navigate(['page2']);
+  }
 }
