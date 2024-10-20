2c270495d3531d2dc56a46244aea2f5861d6131d
RohitLbd <rrlambade_m23@ee.vjti.ac.in>
2024-10-20 09:45:01 +0530

    change in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { 
         path: 'register', component: RegisterComponent,
+    },
+    { 
+        path: 'login', component: LoginComponent,
     },
     // { 
     //     path: 'about', component: AboutComponent,
