2f18bff41be7382c2ffa52823a42f9d142fc3de1
RohitLbd <rrlambade_m23@ee.vjti.ac.in>
2024-10-20 12:06:46 +0530

    graphs commit

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './page2.component.css'
 })
 export class Page2Component {
+  constructor(private router:Router){}
 
+  gui(data:any){
+    console.log(data)
+    this.router.navigate(['gui'],{ state: { data } });
+  }
 }
