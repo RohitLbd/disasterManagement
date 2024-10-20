a5619ab6c7523a1e971f4f31bc06f787303196c9
RohitLbd <rrlambade_m23@ee.vjti.ac.in>
2024-10-20 10:21:04 +0530

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { 
+    path: '', component: LandingPageComponent,
+},
   { 
     path: 'register', component: RegisterComponent,
 },
