da1ed35154a42f545330e985ea5460c9d537a6d5
Zakieexe <mzkhan_m24@el.vjti.ac.in>
2024-10-20 10:20:28 +0530

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
+    
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
   ]
 })
 export class AppModule { }
